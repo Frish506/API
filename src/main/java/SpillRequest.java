c07a41a2c5bd69db19bb92bc7a1adbf2974bebbc
Henry Frishman <henry.frishman@gmail.com>
2018-04-18 02:14:39 -0400

    Redid run so it's singular

--- src/main/java/SpillRequest.java
+++ src/main/java/SpillRequest.java
@@ -1,26 +1,40 @@
 
 
-import sun.applet.Main;
-
+import PhoneFeature.Server;
+import Stages.StagesClass;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 
 public class SpillRequest {
-    public static int x, y, windowWidth, windowLength;
-    public static String cssPath, destNodeID, originNodeID;
+
 
     public SpillRequest() {
 
     }
 
-    public void run(int xcoord, int ycoord, int windowWidth, int windowLength, String cssPath, String destNodeID, String originNodeID) {
-        this.x = xcoord;
-        this.y = ycoord;
-        this.windowWidth = windowWidth;
-        this.windowLength = windowLength;
-        this.cssPath = cssPath;
-        this.destNodeID = destNodeID;
-        this.originNodeID = originNodeID;
-        String[] a = new String[0];
-        Main.main(a);
+    public void run(int xcoord, int ycoord, int windowWidth, int windowLength, String cssPath, String destNodeID, String originNodeID) throws Exception {
+
+        Server s = new Server();
+        s.runServer();
+
+        StagesClass.mainStage = new Stage();
+        ///////////////////Set up Scenes Here////////////
+
+        Parent ServiceRequestScreen = FXMLLoader.load(getClass().getResource("FXMLFiles/ServiceRequest/SpillTab.fxml"));//These files need to be in the resource folder!
+        StagesClass.serviceScene = new Scene(ServiceRequestScreen, windowWidth, windowLength);
+        StagesClass.serviceScene.getStylesheets().add(cssPath);
+        StagesClass.serviceStage.initModality(Modality.APPLICATION_MODAL);
+
+        StagesClass.mainStage.setX(xcoord);
+
+        StagesClass.mainStage.setY(ycoord);
+
+
+        StagesClass.mainStage.setScene(StagesClass.serviceScene);
+        StagesClass.mainStage.show();
     }
 }
